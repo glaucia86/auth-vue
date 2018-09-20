97fb39070dd6d5a8f4b3cc3ff6e1e2bdab09ca10
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 13:38:35 -0300

    Desenvolvimento do script para o componente 'Register.vue'

--- auth-vue-app/src/components/Register.vue
+++ auth-vue-app/src/components/Register.vue
@@ -38,7 +38,57 @@
 </template>
 
 <script>
-    
+    export default {
+        props: ['nextUrl'],
+        data() {
+            return {
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+                is_admin: null
+            }
+        },
+        methods: {
+            handleSubmit(e) {
+                e.preventDefault();
+                if (this.password === this.password_confirmation && this.password.length > 0) {
+                    let url = 'http://localhost:3000/register'
+                if (this.is_admin != null || this.is_admin == 1) 
+                    url = 'http://localhost:3000/register-admin'
+
+                    this.$http.post(url, {
+                        name: this.name,
+                        email: this.email,
+                        password: this.password,
+                        is_admin: this.is_admin
+                    })
+                    .then(response => {
+                        localStorage.setItem('user', JSON.stringify(response.data.user))
+                        localStorage.setItem('jwt', response.data.token)
+
+                        if (localStorage.getItem('jwt') != null) {
+                            this.$emit('loggedIn')
+
+                            if (this.$route.params.nextUrl != null) {
+                                this.$router.push(this.$route.params.nextUrl)
+                            } else {
+                                this.$router.push('/')
+                            }
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+                } else {
+                    this.password = ''
+                    this.passwordConfirmation = ''
+
+                    return alert('As senhas não conferem. Tente novamente!');
+                }
+            }
+        }
+    }    
 </script>
 
 <style scoped>
