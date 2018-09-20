ad9ea242bfd1f8f06922110e71c38f8efaf075f3
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 13:50:53 -0300

    Inclusão do pacote axios na aplicação.

--- auth-vue-app/src/main.js
+++ auth-vue-app/src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
