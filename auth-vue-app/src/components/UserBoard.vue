d117895ee06279b4c2a4bf9ab2a72bd224068e12
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 14:49:16 -0300

    Atualização no código

--- auth-vue-app/src/components/UserBoard.vue
+++ auth-vue-app/src/components/UserBoard.vue
@@ -1,36 +1,37 @@
 <template>
-    <div class="hello">
+    <div class='hello'>
         <h1>Seja Bem-Vindo(a) a Página de Usuários!</h1>
         <h2>{{ msg }}</h2>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                msg: 'Você está logado!'
-            }
-        }
+export default {
+  data () {
+    return {
+      msg: 'Você está logado!'
     }
+  }
+}
 </script>
 
 <style scoped>
-    h1, h2 {
-        font-weight: norma;
-    }
+h1,
+h2 {
+  font-weight: norma;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-    a {
-        color: #42b983;
-    }
+a {
+  color: #42b983;
+}
 </style>
--- auth-vue-app/src/router/index.js
+++ auth-vue-app/src/router/index.js
@@ -71,7 +71,7 @@ router.beforeEach((to, from, next) => {
     } else {
       let user = JSON.parse(localStorage.getItem('user'))
       if (to.matched.some(record => record.meta.is_admin)) {
-        if (user.is_admin == 1) {
+        if (user.is_admin === 1) {
           next()
         } else {
           next({ name: 'userboard' })
