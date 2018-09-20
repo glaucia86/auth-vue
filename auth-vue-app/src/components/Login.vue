6babd12ff69f89ab9a5831bceee50e546a52c91a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 11:52:55 -0300

    Desenvolvimento do componente: 'Login'

---
+++ auth-vue-app/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h4>Acesso ao Login</h4>
+        <form>
+            <label for="email">Endereço de E-mail</label>
+            <div>
+                <input id="email" type="email" v-model="email" required autofocus>
+            </div>
+
+            <div>
+                <label for="password">Senha</label>
+                <div>
+                    <input id="password" type="password" v-model="password" required>
+                </div>
+            </div>
+            <div>
+                <button type="submit" @click="handleSubmit">Login</button>
+            </div>
+        </form>
+    </div>    
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+
+</style>
+
