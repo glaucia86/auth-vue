80013db2e92a728ed83f9bd3c1182c781ede659a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 10:50:37 -0300

    Desenvolvimento do arquivo 'db.js'

--- auth-vue-app/server/db.js
+++ auth-vue-app/server/db.js
@@ -15,7 +15,50 @@ class Db {
     this.createTable()
   }
 
-  createTable() {
-    const sql = ``  
+  // Aqui estamos criando uma tabela para a aplicação:
+  createTable () {
+    const sql = `
+      create table if not exists user (
+        id integer primary key,
+        name text,
+        email text unique
+        user_pass text,
+        is_admin integer)`
+    return this.db.run(sql)
+  }
+
+  selectByEmail (email, calllback) {
+    return this.db.get(
+      `select * from user where = ?`,
+      [email], (err, row) => {
+        calllback(err, row)
+      })
+  }
+
+  insertAdmin (user, calllback) {
+    return this.db.run(
+      `insert into user(name, email, user_pass, is_admin)
+       values (?, ?, ?, ?)`,
+      user, (err) => {
+        calllback(err)
+      })
+  }
+
+  selectAll (calllback) {
+    return this.db.all(
+      `select * from user`,
+      (err, rows) => {
+        calllback(err, rows)
+      })
+  }
+
+  insert (user, calllback) {
+    return this.db.run(
+      `insert into user (name, email, user_pass) values (?, ?, ?)`,
+      user, (err) => {
+        calllback(err)
+      })
   }
 }
+
+module.exports = Db
