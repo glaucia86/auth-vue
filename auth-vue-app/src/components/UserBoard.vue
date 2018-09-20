741bc057a4c5f9c023dfdde77ff7808af39acc9b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 13:45:39 -0300

    Desenvolvimento do componente: 'UserBoard.vue'

---
+++ auth-vue-app/src/components/UserBoard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="hello">
+        <h1>Seja Bem-Vindo(a) a Página de Usuários!</h1>
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'Você está logado!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1, h2 {
+        font-weight: norma;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
