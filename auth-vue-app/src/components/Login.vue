e4b048784320fba3194a552be40e4391c8f99573
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 12:07:06 -0300

    Desenvolvimento do script que fará ligação com o server

--- auth-vue-app/src/components/Login.vue
+++ auth-vue-app/src/components/Login.vue
@@ -36,8 +36,24 @@
                         email: this.email,
                         password: this.password
                     })
+                    // Bloco que fará ligação com o server:
                     .then(response => {
-
+                        let is_admin = response.data.user.is_admin
+                        localStorage.setItem('user', JSON.stringify(response.data.user))
+                        localStorage.setItem('jwt', response.data.token)   
+                        
+                        if (localStorage.getItem('jwt') != null) {
+                            this.$emit('loggedIn')
+                            if (this.$route.params.nextUrl != null) {
+                                this.$router.push(this.$route.params.nextUrl)
+                            } else {
+                                if (is_admin == 1) {
+                                    this.$router.push('admin')
+                                } else {
+                                    this.$router.push('dashboard')
+                                }
+                            }
+                        }
                     })
                     .catch ((error) => {
                         console.error(error.response);
