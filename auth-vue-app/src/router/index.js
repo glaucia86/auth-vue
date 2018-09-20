3b67a43cb0423e74345e32e2f697a77dd51e45fb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-20 11:14:22 -0300

    Desenvolvimento das rotas da aplicação no vue-router.

--- auth-vue-app/src/router/index.js
+++ auth-vue-app/src/router/index.js
@@ -1,15 +1,59 @@
+/**
+ * Arquivo: src/router/index.js
+ * Data: 20/09/2018
+ * Descrição: arquivo responsável por definir todas as rotas da aplicação. (frontend)
+ * Author: Glaucia Lemos
+ */
+
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import UserBoard from '@/components/UserBoard'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
-  ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'userboard',
+      component: UserBoard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        is_Admin: true
+      }
+    }]
 })
